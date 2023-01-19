7525096f85370d0f8c574602a2be13be11daa67f
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-01-19 20:23:38 +0100

    solution task 2 in module 101

--- src/main/kotlin/lms_126/Task2.kt
+++ src/main/kotlin/lms_126/Task2.kt
@@ -2,5 +2,11 @@ package lms_126
 
 fun task2(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        INPUT b1,b2,area
+        INPUT b1 = base of rectangle
+        INPUT b2 = height of rectangle
+        calculate area by multiply b1 * b2
+        OUTPUT area of rectangle
+    """
 }
