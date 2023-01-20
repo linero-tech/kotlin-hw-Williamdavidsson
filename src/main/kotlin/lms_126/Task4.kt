0fda39250450e9a072839c35bb2b08166456b6c3
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-01-20 11:47:44 +0100

    update task4 M101

--- src/main/kotlin/lms_126/Task4.kt
+++ src/main/kotlin/lms_126/Task4.kt
@@ -3,10 +3,22 @@ package lms_126
 fun task4(): String {
 
     return """
-        INPUT symbols x,y
-        INPUT x equal 5
-        INPUT y equal 2
-        INPUT x greater value than y
-        OUTPUT x>y
+        Input num1: 10
+        Input num2: 20
+
+        Output: Maximum = 20
+        READ n1,n2
+        IF n1>n2
+	    WRITE "n1 is Maximum"
+        ELSE
+	    WRITE "n2 is Maximum"
+        ENDIF
+        Step 1: Start
+        Step 2: Declare variables n1, n2
+        Step 3: If n1 is greater than n2, 
+		Display "n1 is Maximum"
+	else
+		Display "n2 is Maximum"
+    Step 4: Stop      
     """
 }
