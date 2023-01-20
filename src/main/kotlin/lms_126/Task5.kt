bfee652b665af92ee694d5d9ebcbb1977f158196
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-01-20 22:01:49 +0100

    solution task 5 M101

--- src/main/kotlin/lms_126/Task5.kt
+++ src/main/kotlin/lms_126/Task5.kt
@@ -2,5 +2,12 @@ package lms_126
 
 fun task5(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+       days_of_the_week = ["Monday", "Tuseday", "Wendesday", "Thursday", "Friday", "Saturday", "Sunday"]
+       input: x
+       if x >= 1 and x >= 7:
+       print (days_of_week[x - 1])
+       else: print("Invalid input. X must be between 1 and 7, inclusive.")
+    """
+
 }
