b5d3f7976584b00d5053984f35c46293749ee0b3
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-01-21 13:03:36 +0100

    update task 6 M101

--- src/main/kotlin/lms_126/Task6.kt
+++ src/main/kotlin/lms_126/Task6.kt
@@ -3,13 +3,10 @@ package lms_126
 fun task6(): String {
 
     return """
-        for i = 1 to 100
-    if i is divisible by 5
-        print i
-    end if
-end for
-
-
-    """.trimIndent()
+      input: i = 1 to 100
+    if : i is divisible by 5
+      output:  print i
+    end
+    """
 
 }
