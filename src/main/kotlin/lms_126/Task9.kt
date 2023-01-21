5d412c2a10c85f8630d596cff884da5c1efa08a2
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-01-21 17:10:21 +0100

    update task9.M101

--- src/main/kotlin/lms_126/Task9.kt
+++ src/main/kotlin/lms_126/Task9.kt
@@ -3,13 +3,12 @@ package lms_126
 fun task9(): String {
 
     return """
-        INPUT withdrawMoney(amount: Double) {
-        if (amount > accountBalance) {
-        println("Insufficient funds.")
-        } else {
-        accountBalance -= amount
-        println("Withdrawal successful."
-        println("Remaining balanc: $ accountbalanc")
-        }
+        INPUT withdraw money = amount money
+        INPUT account balanc => amount money
+        if (amount > account Balance)
+        Output ("Insufficient funds.")
+        Else account balance = amount money
+        Output "Withdrawal successful"
+        Output "Remaining balanc: $ account balanc"
     """
 }
