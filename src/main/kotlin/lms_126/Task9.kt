46aca29798738d9a10fdefa21e94f7a49f1e5322
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-01-21 16:47:52 +0100

    sol.1 task9. M101

--- src/main/kotlin/lms_126/Task9.kt
+++ src/main/kotlin/lms_126/Task9.kt
@@ -2,5 +2,14 @@ package lms_126
 
 fun task9(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        INPUT withdrawMoney(amount: Double) {
+        if (amount > accountBalance) {
+        println("Insufficient funds.")
+        } else {
+        accountBalance -= amount
+        println("Withdrawal successful."
+        println("Remaining balanc: $ accountbalanc")
+        }
+    """
 }
