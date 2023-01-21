d1f667fe138e4133b0c5d2e6a164c0aaaf84e17b
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-01-21 13:44:26 +0100

    sol.Task7.1M101

--- src/main/kotlin/lms_126/Task7.kt
+++ src/main/kotlin/lms_126/Task7.kt
@@ -2,5 +2,9 @@ package lms_126
 
 fun task7(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+       input : (i) <= 100
+        if (i % 2 == 0)
+       output : (i) 
+    """
 }
