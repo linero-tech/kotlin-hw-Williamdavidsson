90ce173a722d39d2f9b598e587e88ec8726feaf2
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-01-21 12:44:29 +0100

    Solution 1 task6 M101

--- src/main/kotlin/lms_126/Task6.kt
+++ src/main/kotlin/lms_126/Task6.kt
@@ -2,5 +2,13 @@ package lms_126
 
 fun task6(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        INPUT: printMultiplesOfFive() {
+            for (i in 5..100 step 5) {
+                println(i)
+            }
+        }
+
+    """.trimIndent()
+
 }
