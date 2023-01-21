03e15a9e22ef2c131b9614317acabec9ffa7c4b8
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-01-21 12:27:52 +0100

    update3 task 5 M101

--- src/main/kotlin/lms_126/Task5.kt
+++ src/main/kotlin/lms_126/Task5.kt
@@ -4,10 +4,11 @@ fun task5(): String {
 
     return """
        input: days_of_the_week = ["Monday", "Tuseday", "Wendesday", "Thursday", "Friday", "Saturday", "Sunday"]
-       input: x one_day_of_week
+       input: x < 8
        if x >= 1 and x >= 7
        print: (days_of_week[x - 1])
-       else print("Invalid input. X must be between 1 and 7, inclusive.")
+       else 
+       print: error
      """
 
 }
