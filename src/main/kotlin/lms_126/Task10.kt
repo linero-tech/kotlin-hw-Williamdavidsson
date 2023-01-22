41290bc65eaa2505e3bbe6c7113f812a145136e7
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-01-22 22:29:09 +0100

    sol.task10.M101

--- src/main/kotlin/lms_126/Task10.kt
+++ src/main/kotlin/lms_126/Task10.kt
@@ -2,5 +2,10 @@ package lms_126
 
 fun task10(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        INPUT createAccount(email, password)
+        IF (password.length < 6)
+        print("Error: password must be at least 6 charactar.)
+        ELSE print("Account created")
+    """
 }
