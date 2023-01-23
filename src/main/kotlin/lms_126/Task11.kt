c19ad2c3f17652572a84903c7e379c956295d6eb
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-01-23 15:26:59 +0100

    update2 task 11.M101

--- src/main/kotlin/lms_126/Task11.kt
+++ src/main/kotlin/lms_126/Task11.kt
@@ -3,11 +3,15 @@ package lms_126
 fun task11(): String {
 
     return  """
-        INPUT convert temp
-        IF temp [-1] == "C"
-        return (float(temp[-1]) - 32) * 5/9
-            ELSE
-            return "Invalid input"
-            temp = input("Enter temp (e.g. 20C, 70F)")
-            OUTPUT (convert temp (temp)) """
+        INPUT convert temperature
+        IF temp [-1] == "C":
+        temp in Fahrenheit = (float(temp[:-1]) * 9/5 + 32
+        return str(temp in fahrengeit) + "F
+        IF temp[-1] == "F":
+        temp in celsius = (float(temp[:-1]) - 32) * 5/9
+        return str(temp in celsius) + "c"
+            ELSE:
+            return "Invalid input":
+            temp = input("Enter temp (e.g. 20C, 70F): ")
+            OUTPUT (convert_temp (temp)) """
 }
