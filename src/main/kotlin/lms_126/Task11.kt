264b52045a01cc0a7993f11ae7bf54479f6fd1fa
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-01-23 15:03:19 +0100

    sol,task11.M101

--- src/main/kotlin/lms_126/Task11.kt
+++ src/main/kotlin/lms_126/Task11.kt
@@ -2,5 +2,12 @@ package lms_126
 
 fun task11(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        INPUT convert temp
+        IF temp [-1] == "C"
+        return (float(temp[-1]) - 32) * 5/9
+            ELSE
+            return "Invalid input"
+            temp = input("Enter temp (e.g. 20C, 70F)")
+            OUTPUT (convert temp (temp)) """
 }
