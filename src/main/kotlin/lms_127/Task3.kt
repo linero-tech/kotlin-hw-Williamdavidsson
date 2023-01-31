e0b3d804d8e20e4f305c9d505cb597290a760313
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-01-31 15:27:34 +0100

    task3.M102

--- src/main/kotlin/lms_127/Task3.kt
+++ src/main/kotlin/lms_127/Task3.kt
@@ -2,5 +2,13 @@ package lms_127
 
 fun task3(radius: Double): Double {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val pi = 3.1416
+    val result = 2 * pi * radius;
+    return result
+
 }
+fun main() {
+    val answer = task3(radius = 2.0)
+    print(answer)
+}
+
