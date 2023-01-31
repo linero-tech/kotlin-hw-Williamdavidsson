888df448d34372135a1a2938290fe3e1fc8ecce6
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-01-31 14:12:49 +0100

    update1task1.M102

--- src/main/kotlin/lms_127/Task1.kt
+++ src/main/kotlin/lms_127/Task1.kt
@@ -1,7 +1,8 @@
 package lms_127
 
 fun task1() {
-    var country = "Panama"
-    var capital = "Panama"
-    var currency = "USD"
+    val country = "Panama"
+    val capital = "Panama"
+    val currency = "USD"
+    return
 }
