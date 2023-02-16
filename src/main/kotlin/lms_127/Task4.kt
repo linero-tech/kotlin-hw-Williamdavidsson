803f51d789286b18c1b6abaf6421bc9c92d90abc
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-02-16 17:22:12 +0100

    Update2 Task4M102

--- src/main/kotlin/lms_127/Task4.kt
+++ src/main/kotlin/lms_127/Task4.kt
@@ -5,6 +5,6 @@ fun task4(base: Double, height: Double): Double {
     return result 
 }
 fun main() {
-val result = task4(base = 5.0, height = 10.0
+val result = task4('base = 5.0,  height = 10.0
 println(result)
 } 
