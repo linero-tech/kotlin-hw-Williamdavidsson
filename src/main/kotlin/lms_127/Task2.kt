62944f7fbb6625edfc8db151bc2ca72abbb3d2e6
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-02-19 20:50:48 +0100

    UPDATE TASK2 M102

--- src/main/kotlin/lms_127/Task2.kt
+++ src/main/kotlin/lms_127/Task2.kt
@@ -6,5 +6,5 @@ fun main () {
     val name = "Jane"
     val country = "Sweden"
     val month: String = "June"
-    println("Hello $name, you are from $country, and you were born in $month")
+    println("Hello $name, you are from $country, and you were born in $month.")
 }
