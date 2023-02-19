860ae03d07f2808c557fe9a981966fee9cd9e823
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-02-19 23:26:43 +0100

    Task5,M102

--- src/main/kotlin/lms_127/Task5.kt
+++ src/main/kotlin/lms_127/Task5.kt
@@ -2,11 +2,15 @@ package lms_127
 
 fun task5(valueForA: Int, valueForB: Int): Pair<Int, Int> {
     // Do not change or delete these variables
+
     var a = valueForA
     var b = valueForB
-
-    TODO("Your solutions goes in here.")
-
+    var valueForA = 1
+    var valueForB = 2
     // Do not erase or change this statement
     return Pair(a, b)
+}
+fun main() {
+    println(task5(valueForA = 2, valueForB = 1))
+    println(task5(valueForA = 1, valueForB = 2))
 }
