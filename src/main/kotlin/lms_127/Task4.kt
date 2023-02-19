4bd5f536abe86b9c6b0992f7087369e0a8305af9
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-02-19 20:33:22 +0100

    update task4M101

--- src/main/kotlin/lms_127/Task4.kt
+++ src/main/kotlin/lms_127/Task4.kt
@@ -2,9 +2,9 @@ package lms_127
 
 fun task4(base: Double, height: Double): Double {
     val result = 0.5 * base * height
-    return result 
+    return result
 }
 fun main() {
-val result = task4(base = 5.0,  height = 10.0)
-println(result)
-} 
+    val result = task4(base = 5.0, height = 10.0)
+    println(result)
+}
