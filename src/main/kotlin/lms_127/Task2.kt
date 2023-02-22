dc7151996a2c40e698961d980139926561aba967
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-02-22 14:25:35 +0100

    T2M102

--- src/main/kotlin/lms_127/Task2.kt
+++ src/main/kotlin/lms_127/Task2.kt
@@ -3,8 +3,8 @@ package lms_127
 import java.time.Month
 
 fun main () {
-    val name = "Jane"
+    val name = "William"
     val country = "Sweden"
     val month: String = "June"
-    println("Hello $name, you are from $country, and you were born in $month.")
+    println("Hello $name, you are from $country, and you were born in $month")
 }
