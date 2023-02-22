ba537b67096f6c6c0a27b30185dc56521a161f76
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-02-22 18:33:40 +0100

    TASK8102

--- src/main/kotlin/lms_127/Task8.kt
+++ src/main/kotlin/lms_127/Task8.kt
@@ -1,6 +1,11 @@
 package lms_127
 
 fun task8(sentence: String, character: Char): Int {
+    return sentence.count {it == character}
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+}
+fun main()  {
+    println(task8(sentence = "I code in Kotlin", character = 'I'))
+    println(task8(sentence = " I code in Kotlin", character = 'i'))
+    println(task8(sentence = " I code in Kotlin", character = 'z'))
 }
