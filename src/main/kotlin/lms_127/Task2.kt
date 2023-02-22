03ec80b1becf6cced42226561dd5df433ca2351f
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-02-22 15:30:52 +0100

    T2M2

--- src/main/kotlin/lms_127/Task2.kt
+++ src/main/kotlin/lms_127/Task2.kt
@@ -1,10 +1,8 @@
 package lms_127
-
-import java.time.Month
 fun task2() {
     val name = "William"
     val country = "Sweden"
-    val month = Month.JUNE
+    val month = "June"
     println("Hello $name, you are from $country, and you were born in $month")
 }
 fun main() {
