8c6330d983a7c50335d1d70f8d171dafa4b2d8c0
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-02-22 17:40:19 +0100

    T1M2

--- src/main/kotlin/lms_127/Task1.kt
+++ src/main/kotlin/lms_127/Task1.kt
@@ -1,9 +1,9 @@
 package lms_127
 
 fun task1() : String {
-    val countryName = "Sweden"
-    val capitalName = "Stokholm"
-    val currency = "SEK"
+    val countryName = "Panama"
+    val capitalName = "Panama City"
+    val currency = "USD"
     val sentence = "$countryName, $capitalName, $currency"
     return sentence
 }
