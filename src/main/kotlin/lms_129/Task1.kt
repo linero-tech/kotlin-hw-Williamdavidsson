5a804ab1a6d447cd7f45b71797c165f285bfc04f
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-03-03 21:51:03 +0100

    T1M103

--- src/main/kotlin/lms_129/Task1.kt
+++ src/main/kotlin/lms_129/Task1.kt
@@ -1,6 +1,19 @@
 package lms_129
 
 fun task1(a: Int, b: Int): Int {
-
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = 0
+    if (a >= b){
+        result = 0
+    } else {
+for (i in a..b) {
+    result += i
+        }
+    }
+    return result
+}
+fun main(){
+    val a = 1
+    val b = 5
+    val result = task1(a, b)
+    print("result is $result")
 }
