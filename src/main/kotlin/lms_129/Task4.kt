a52e96d3e2837c3cefc5122c2a0b110058b4c500
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-03-04 12:38:09 +0100

    T4M103

--- src/main/kotlin/lms_129/Task4.kt
+++ src/main/kotlin/lms_129/Task4.kt
@@ -1,6 +1,17 @@
 package lms_129
 
 fun task4(): Int {
+    var result = 0
+    for (i in 1..1000){
+       if(i % 9 == 0){
+           result += i
+       }
+    }
+    return result
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+}
+fun main(){
+    val result = task4()
+    println(result)
+
 }
