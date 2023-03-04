c7a1a965cc3c461c22c36cf1daacc4330114c945
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-03-04 01:05:50 +0100

    T5M103

--- src/main/kotlin/lms_129/Task5.kt
+++ src/main/kotlin/lms_129/Task5.kt
@@ -1,6 +1,19 @@
 package lms_129
 
 fun task5(sentence: String): Int {
+        var result = 0
+        for (char in sentence) {
+            result += 1
+        }
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
-}
+        println("The sentence '$sentence' has $result characters.")
+
+        return result
+    }
+    fun main () {
+        println(task5(sentence = "I love GBG"))
+    }
+
+
+
+
