59ebe7c092dbe9a2e7cd855146faa73f3393f431
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-03-05 21:51:41 +0100

    T8M103

--- src/main/kotlin/lms_129/Task8.kt
+++ src/main/kotlin/lms_129/Task8.kt
@@ -1,6 +1,16 @@
 package lms_129
 
 fun task8(number: Int): Int {
-
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
-}
+        var result = 0
+        var n = number
+        while (n > 0) {
+            result += n % 10
+            n /= 10
+        }
+        return result
+    }
+    fun main(){
+        val number = 123
+        val result = task8(number)
+        println("the sum of the digits in $number is $result")
+    }
