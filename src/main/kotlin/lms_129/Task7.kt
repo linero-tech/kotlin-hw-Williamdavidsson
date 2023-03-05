3e594a37594b9e5d9229c0d5bf77e8a1313f10e7
William Davidsson <122484007+Williamdavidsson@users.noreply.github.com>
2023-03-05 21:13:00 +0100

    T7M103

--- src/main/kotlin/lms_129/Task7.kt
+++ src/main/kotlin/lms_129/Task7.kt
@@ -1,6 +1,25 @@
 package lms_129
 
 fun task7(a: Int, b: Int): Int {
-
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = 1
+    if (a == 0 && b == 0) {
+        return result
+    }
+    for (i in 1..b) {
+        result *= a
+    }
+    return result
 }
+fun main() {
+    val a = 2
+    val b = 3
+val result = task7(a, b)
+    println("$a raised to the power of $b is the $result")
+}
+
+
+
+
+
+
+
